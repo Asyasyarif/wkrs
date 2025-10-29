36cf7cea707515e0cba259f681b023b350b2d4cf
ARIF <asya.syarif94@gmail.com>
2025-10-29 23:11:53 +0700

    first

---
+++ script.js
@@ -0,0 +1,108 @@
+const menuItems = [
+    {
+        id: 0,
+        name: "Perkedel",
+        image: "assets/perkedel2.png",
+        price: 3000,
+        type: "makanan"
+    },
+    {
+        id: 1,
+        name: "Sayur Tahu",
+        image: "assets/sayur_tahu.png",
+        price: 5000,
+        type: "makanan"
+    },
+    {
+        id: 2,
+        name: "Ayam Goreng",
+        image: "assets/ayam_goreng.png",
+        price: 12000,
+        type: "makanan"
+    },
+    {
+        id: 3,
+        name: "Mie Goreng",
+        image: "assets/sayur_sop.png",
+        price: 5000,
+        type: "makanan"
+    },
+    {
+        id: 4,
+        name: "Tahu",
+        image: "assets/tahu.png",
+        price: 3000,
+        type: "makanan"
+    },
+    {
+        id: 5,
+        name: "Jengkol",
+        image: "assets/jengkol.png",
+        price: 5000,
+        type: "makanan"
+    },
+    {
+        id: 6,
+        name: "Telur Balado",
+        image: "assets/telor_balado.png",
+        price: 6000,
+        type: "makanan"
+    },
+    {
+        id: 7,
+        name: "Kangkung",
+        image: "assets/kangkung.png",
+        price: 5000,
+        type: "makanan"
+    },
+    {
+        id: 8,
+        name: "Tahu Bacem",
+        image: "assets/tahu_bacem.png",
+        price: 3000,
+        type: "makanan"
+    },
+    {
+        id: 9,
+        name: "Sosis",
+        image: "assets/sosis.png",
+        price: 6000,
+        type: "makanan"
+    },
+    {
+        id: 10,
+        name: "Terong",
+        image: "assets/terong.png",
+        price: 5000,
+        type: "makanan"
+    },
+    {
+        id: 11,
+        name: "Tempe Goreng",
+        image: "assets/tempe_goreng.png",
+        price: 3000,
+        type: "makanan"
+    },
+    {
+        id: 12,
+        name: "Es Teh Manis",
+        image: "assets/es_teh_manis.png",
+        price: 5000,
+        type: "minuman"
+    },
+    {
+        id: 13,
+        name: "Es Jeruk",
+        image: "assets/es_jeruk.png",
+        price: 7000,
+        type: "minuman"
+    },
+    {
+        id: 14,
+        name: "Teh Hangat",
+        image: "assets/teh_hangat.png",
+        price: 2000,
+        type: "minuman"
+    }
+];
+
